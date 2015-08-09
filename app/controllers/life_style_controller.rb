a16263df510c5af7a18cc935732b2b8e391a50da
tol3 <tol3@me.com>
2015-08-09 17:10:48 +0700

    update all layout

--- app/controllers/life_style_controller.rb
+++ app/controllers/life_style_controller.rb
@@ -3,14 +3,14 @@ class LifeStyleController < ApplicationController
   impressionist :actions=>[:index]
 
   def index
-    @head = "Life Style"
+    @head = "LifeStyle"
     @url = "lifestyle"
     @news = News.life_style.publish.reverse_order.page(params[:page]).per(16)
     @first = News.life_style.publish.reverse_order.first
   end
 
   def show
-    @title = "Life Style"
+    @title = "LifeStyle"
     @url = "lifestyle"
     @news = News.life_style.find(params[:id])
     @head = @news.title

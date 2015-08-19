17380e20e7c6e946d23a0f706c0aa0dda407a4c2
tol3 <tol3@me.com>
2015-08-19 13:14:06 +0700

    fix bytelength

--- app/admin/news.rb
+++ app/admin/news.rb
@@ -133,15 +133,15 @@ ActiveAdmin.register News do
       page_graph = Koala::Facebook::API.new(user.page_token)
 
       link = "http://www.wow2mouth.com/#{news.getNewsUrl(news.category.id)}/#{news.id}"
-      article = news.teaser.first(200) + "... \nอัพเดทข่าวดาราได้ที่ >> http://www.wow2mouth.com\n" + news.tag_hash
-
+      article = limit_bytesize(news.teaser, 120) + "... \nอัพเดทข่าวได้ที่ http://www.wow2mouth.com\n" + limit_bytesize(news.tag_hash, 50)
+      # raise article.inspect
       page_graph.put_wall_post( article,
         {
           name: news.title,
           link: link,
           caption: "WOW2MOUTH.COM",
           description: news.teaser.first(120),
-          picture: "http://www.wow2mouth.com"+news.cover.url
+          picture: "http://www.wow2mouth.com"+news.cover.url(:mini)
         }
       )
 
@@ -150,6 +150,27 @@ ActiveAdmin.register News do
 
       redirect_to admin_news_path, notice: "Facebook has Posted!"
     end
+
+    def limit_bytesize(str, size)
+      str.encoding.name == 'UTF-8' or raise ArgumentError, "str must have UTF-8 encoding"
+
+      # Change to canonical unicode form (compose any decomposed characters).
+      # Works only if you're using active_support
+      str = str.mb_chars.compose.to_s if str.respond_to?(:mb_chars)
+
+      # Start with a string of the correct byte size, but
+      # with a possibly incomplete char at the end.
+      new_str = str.byteslice(0, size)
+
+      # We need to force_encoding from utf-8 to utf-8 so ruby will re-validate
+      # (idea from halfelf).
+      until new_str[-1].force_encoding('utf-8').valid_encoding?
+        # remove the invalid char
+        new_str = new_str.slice(0..-2)
+      end
+      new_str
+    end
   end
+
 
 end

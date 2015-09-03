f643d936584f7fc3a852682dfb674220a051d0a3
tol3 <tol3@me.com>
2015-09-04 03:15:31 +0700

    fix puma

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,7 +1,7 @@
-PumaWorkerKiller.start
+# PumaWorkerKiller.start
 
-PumaWorkerKiller.config do |config|
-  config.ram           = 4096# mb
-  config.frequency     = 30    # seconds
-  config.percent_usage = 0.80
-end
+# PumaWorkerKiller.config do |config|
+#   config.ram           = 4096# mb
+#   config.frequency     = 30    # seconds
+#   config.percent_usage = 0.80
+# end

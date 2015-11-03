7c65c2e9743691f4139adad59fc5d95291d32755
o0KuReNai0o <demon.nae@gmail.com>
2015-11-03 15:34:30 +0700

    Ready

--- config/initializers/puma_worker_killer.rb
+++ config/initializers/puma_worker_killer.rb
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-PumaWorkerKiller.start
-
-PumaWorkerKiller.config do |config|
-  config.ram           = 4096# mb
-  config.frequency     = 30    # seconds
-  config.percent_usage = 0.80
-end
-=======
 # PumaWorkerKiller.start
 
 # PumaWorkerKiller.config do |config|
@@ -14,4 +5,3 @@ end
 #   config.frequency     = 30    # seconds
 #   config.percent_usage = 0.80
 # end
->>>>>>> 9cd22814fe488c5c934ddb88f85c5ba0793ea9aa
